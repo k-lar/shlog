f23f736cd94709d11a3c0f41b396c68711db088e
K_lar <sprogarzan@gmail.com>
2023-04-28 09:46:45 +0200

    Adding shows, better menus

--- src/main.go
+++ src/main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-    // "fmt"
+    "fmt"
     "os"
     "log"
     "bufio"
@@ -23,19 +23,69 @@ func readFile(path string) ([]string, error) {
     return lines, scanner.Err()
 }
 
+func writeToFile(content string) {
+    f, err := os.OpenFile("shows.txt",
+    os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+    if err != nil {
+        log.Println(err)
+    }
+    defer f.Close()
+    if _, err := f.WriteString(content + "\n"); err != nil {
+        log.Println(err)
+    }
+}
+
+func addShow() {
+    showName, _ := pterm.DefaultInteractiveTextInput.WithMultiLine(false).Show("Enter show name")
+    fmt.Print("\033[1A\033[K")
+    fmt.Print("\033[1A\033[K")
+
+    showSeason, _ := pterm.DefaultInteractiveTextInput.WithMultiLine(false).Show("Enter season")
+    fmt.Print("\033[1A\033[K")
+    fmt.Print("\033[1A\033[K")
+
+    showEpisode, _ := pterm.DefaultInteractiveTextInput.WithMultiLine(false).Show("Enter episode")
+    fmt.Print("\033[1A\033[K")
+    fmt.Print("\033[1A\033[K")
+
+    showTime, _ := pterm.DefaultInteractiveTextInput.WithMultiLine(false).Show("Enter time (optional)")
+    fmt.Print("\033[1A\033[K")
+    fmt.Print("\033[1A\033[K")
+
+    entry := showName + ";S" + showSeason + "E" + showEpisode
+    if showTime != "" {
+        entry = entry + "T" + showTime
+    }
+
+    writeToFile(entry)
+
+    pterm.Println(pterm.LightGreen("Entry added:"))
+    pterm.DefaultTable.WithHasHeader().WithBoxed().WithData(pterm.TableData{
+		{"Name", "Season", "Episode", "Time"},
+		{showName, showSeason, showEpisode, showTime},
+	}).Render()
+}
+
 func fuzzySearchShows() string {
+    fmt.Print("\033[1A\033[K")
+    fmt.Print("\033[1A\033[K")
+    area, _ := pterm.DefaultArea.Start() // Start the Area printer.
+
     shows, err := readFile("shows.txt")
     if err != nil {
-        log.Fatalf("readLines: %s", err)
+        log.Fatalf("readFile: %s", err)
     }
 
     selectedOption, _ := pterm.DefaultInteractiveSelect.WithOptions(shows).Show()
     pterm.Info.Printfln("Selected option: %s", pterm.Green(selectedOption))
+
+    area.Update(selectedOption)
+    area.Stop()
     return selectedOption
 }
 
-
 func menu() {
+    area, _ := pterm.DefaultArea.Start() // Start the Area printer.
     menu_options := []string {
         "[1] - Add show",
         "[2] - View progress",
@@ -46,16 +96,23 @@ func menu() {
 
     selectedOption, _ := pterm.DefaultInteractiveSelect.WithOptions(menu_options).Show()
 
+    area.Update(selectedOption)
+    area.Clear()
+
     switch selectedOption {
 	case "[1] - Add show":
+        fmt.Print("\033[1A\033[K")
+        addShow()
+	case "[2] - View progress":
         fuzzySearchShows()
-	case "[2] - Edit progress":
+	case "[3] - Edit progress":
         fuzzySearchShows()
-	case "[3] - Delete show":
+	case "[4] - Delete show":
         fuzzySearchShows()
-	case "[4] - Quit":
+	case "[5] - Quit":
         os.Exit(0)
 	}
+    area.Stop()
 }
 
 func main() {
